4cbbea3abe5c64a27901e4c93fc764aa9d7aa758
SUDHENDU.HM@GMAIL.COM <SUDHENDU.HM@GMAIL.COM>
2018-09-21 14:49:12 +0530

    config propeties

--- thoughtsServer/src/main/java/com/example/GoodTestApplication.java
+++ thoughtsServer/src/main/java/com/example/GoodTestApplication.java
@@ -4,7 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.example.property.ConfigProperties;
+
+@EnableConfigurationProperties({ConfigProperties.class})
 @SpringBootApplication
 public class GoodTestApplication {
 
