a9bb860cdc9a4aedfd0381ea8787d9fb01d52f56
SUDHENDU.HM@GMAIL.COM <SUDHENDU.HM@GMAIL.COM>
2018-09-26 13:44:32 +0530

    spring boot web jar

---
+++ spring-boot-web-jar/src/main/java/com/springweb/jar/config/Application.java
@@ -0,0 +1,29 @@
+package com.springweb.jar.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@ComponentScan(basePackages = "com.springweb.jar")
+@Import({BeanConfig.class, WebConfig.class})
+//@EnableWebMvc
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer{
+	
+	 private static Class<Application> applicationClass = Application.class;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+}
